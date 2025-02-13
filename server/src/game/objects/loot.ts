6114acdebabef6be362e4a5d542b37d02a26b34a
1contra <retryyourphone@gmail.com>
2025-02-12 18:09:23 -0600

    fix: remove console.log i added for debug

--- server/src/game/objects/loot.ts
+++ server/src/game/objects/loot.ts
@@ -199,7 +199,6 @@ export class Loot extends BaseGameObject {
         super(game, pos);
 
         const def = GameObjectDefs[type];
-        console.log(type)
         assert("lootImg" in def, `Invalid loot type ${type}`);
 
         this.layer = layer;
