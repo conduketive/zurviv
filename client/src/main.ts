73c9a22f8716efdeb9cd1aa2087905ad088974ec
SoyAlguien0 <holayasir@gmail.com>
2025-03-04 18:09:33 +0100

    fix textures for low Res

--- client/src/main.ts
+++ client/src/main.ts
@@ -389,7 +389,7 @@ class Application {
                 this.audioManager,
                 this.config,
             );
-            this.resourceManager.loadMapAssets();
+            this.resourceManager.loadMapAssets("halloween");
             this.input = new InputHandler(document.getElementById("game-touch-area")!);
             this.inputBinds = new InputBinds(this.input, this.config);
             this.inputBindUi = new InputBindUi(this.input, this.inputBinds);
