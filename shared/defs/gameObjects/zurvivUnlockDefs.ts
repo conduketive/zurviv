9101f75a9f0bf6b300396861e98459f48d02cc74
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-07-01 00:38:39 +0100

    fix: duplicate basic outfit in loadout and limit some skins to account only

---
+++ shared/defs/gameObjects/zurvivUnlockDefs.ts
@@ -0,0 +1,48 @@
+export const defaultUnlocks = [
+    "outfitBase",
+    "outfitWhite",
+    "outfitKeyLime",
+    "outfitRed",
+    "outfitKhaki",
+    "outfitDarkShirt",
+    "outfitWoodland",
+];
+
+export const accountUnlocks = [
+    "outfitAqua",
+    "outfitCasanova",
+    "outfitCoral",
+    "outfitMeteor",
+    "outfitWoodsCloak",
+    "outfitIslander",
+    "outfitJester",
+    "outfitNoir",
+    "outfitVerde",
+    "outfitBlueLeaderAged",
+    "outfitPrisoner",
+    "outfitPineapple",
+    "outfitWaterElem",
+    "outfitRedLeaderAged",
+    "outfitCarbonFiber",
+    "outfitRoyalFortune",
+    "outfitDarkGloves",
+];
+
+/*
+Specialty Skins- (to specific player accounts, not all)
+Celestial Garb
+Siberian Assault
+Splintered Wheat
+Tallow's Little Helper
+Tarkhany Regal
+Woodcutter's Wrap
+Cobalt Shell (change the color of cobalt shell to match the color of trees in cobalt mode)
+Desert Camo (change the color of desert camo to match the color of trees in desert and savannah modes)
+Developer Swag
+Discord Moderatr
+Forest Camo (change the color of forest camo to match the color of trees in normal, 50v50, potato, and woods modes)
+Fowl Facade
+PARMA Jumpsuit
+The Core Jumpsuit
+Guille Suit (idk if this is spelled correctly, but this could be used for events in the future)
+*/
