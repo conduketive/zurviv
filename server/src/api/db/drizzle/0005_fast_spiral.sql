2806443d12179d1515f3b28c703f61da043c23be
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-10-22 23:56:29 +0100

    migrations

---
+++ server/src/api/db/drizzle/0005_fast_spiral.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "match_data" ADD COLUMN "times_knocked" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "match_data" ADD COLUMN "downed_players" integer DEFAULT 0 NOT NULL;
