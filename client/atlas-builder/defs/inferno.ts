31ab321cb16de795c87a251d33e52e82c962a2c1
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-10-24 17:32:58 +0100

    remove asset with no edges

--- client/atlas-builder/defs/inferno.ts
+++ client/atlas-builder/defs/inferno.ts
@@ -3,8 +3,8 @@ import type { AtlasDef } from "../atlasDefs";
 export const InfernoAtlas: AtlasDef = {
     compress: true,
     images: [
-        "loot/loot-weapon-double-lasr-gun.svg",
-        "loot/loot-weapon-lasr-gun.svg",
+        // "loot/loot-weapon-double-lasr-gun.svg",
+        // "loot/loot-weapon-lasr-gun.svg",
         // "guns/gun-lasr-gun.svg",
         "map/map-stone-03i.svg",
         "map/map-stone-res-02i.svg",
