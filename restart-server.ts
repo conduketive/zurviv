43eb98da74c0b4a5a8972890be6dbd4852d386c0
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-10-29 04:27:37 +0100

    update restart server script

--- restart-server.ts
+++ restart-server.ts
@@ -5,10 +5,13 @@ import { $ } from "bun";
 try {
     await $`sudo -v`;
     process.chdir("/opt/survev/server");
+    await $`pnpm i`;
     await $`git pull`;
     await $`pnpm run build`;
     await $`sudo systemctl restart survev-game`;
 } catch (error) {
     console.error("Update failed:", error);
     process.exit(1);
+} finally {
+    console.log("Done.")
 }
